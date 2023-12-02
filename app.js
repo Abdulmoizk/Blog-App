eb04e6d1a6cef8393da7adbe7ae567d2a39c0e38
moiz <amkhatri89@gmail.com>
2023-12-02 21:34:10 +0500

    signup page
    web responsive of login and signup page
     signup page added

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 function submit(){
     var userName = document.getElementById("userName");
     var password = document.getElementById("password");
-    if (userName.value.trim() && password.value.trim()) {
-        console.log(userName.value);
-        console.log(password.value);
-        userName.value = "";
-        password.value = "";
-    } else {
-        alert("Please enter value");
+    var data = localStorage.getItem("username");
+    data = JSON.parse(data) || [];
+    
+    var useremail = {
+        userName: userName.value,
+        password: password.value
     }
+    data.push(useremail)
+
+localStorage.setItem("username", JSON.stringify(data));
+
 }
